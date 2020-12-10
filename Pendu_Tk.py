59347c8f0803c5c44bc4880f3bfb34c820271a0d
Alex <alexandre.drevet1@gmail.com>
2020-12-10 11:10:34 +0100

    Avancement

--- Pendu_Tk.py
+++ Pendu_Tk.py
@@ -10,35 +10,55 @@ Created on Wed Dec  9 17:21:05 2020
 """
 Que fait ce programme : Jeu du pendu version graphique
 Qui l'a fait : Alexandre Drevet
-Quand a-t-il été réalisé : 
+Quand a-t-il été réalisé : 10/12/2020
 Que reste-t-il à faire : 
 """
 
 #Importation des bibliothèques nécessaires
-from tkinter import Tk, Label, Button, Canvas, StringVar, Entry
-#from ma_lib_tk import proposition
+from tkinter import Tk, Label, Button, Canvas, StringVar, Entry, Frame
+from ma_lib_tk import proposition, inconnu, mots
 
 #Programme principal
+
+#Création de la fenêtre principale
 Mafenetre = Tk()
 Mafenetre.title('Jeu du pendu')
 labelWelcome = Label(Mafenetre, text = 'Bienvenue sur le jeu du pendu', 
                      fg = 'black')
 labelWelcome.pack()
-Largeur = 300
-Longueur = 300
-Canevas = Canvas(Mafenetre, width = Largeur, height = Longueur, bg = 'grey')
-Canevas.pack(padx = 5, pady = 5)
-#photo = PhotoImage(file = 'bonhomme8.gif')
-#item = Canevas.create_image(80,80, image = photo)
-BoutonLettre = Button(Mafenetre, text = 'Entrez une lettre')
-BoutonLettre.pack(side = 'top', padx = 5, pady = 5)
-Lettre = StringVar()
-Champ = Entry(Mafenetre, textvariable = Lettre, bg = 'white', fg = 'black')
-Champ.focus_set()
-Champ.pack(side = 'left', padx = 5, pady = 5)
 buttonQuitter = Button(Mafenetre, text = 'Quitter le pendu', fg = 'black', 
                        command = Mafenetre.destroy)
 
-buttonQuitter.pack()
+buttonQuitter.pack(side = 'bottom')
+
+#Création d'un widget Frame dnas la fenêtre principale
+Frame1 = Frame(Mafenetre, relief = 'groove')
+Frame1.pack(side = 'top', padx = 10, pady = 10)
+
+#Création d'un deuxième widget Frame dans la fenêtre principale
+Frame2 = Frame(Mafenetre, relief = 'groove')
+Frame2.pack(side = 'bottom', padx = 10, pady = 10)
+
+Label(Frame1, text = 'Mot à deviner : ').pack(side = 'left')
+Mots = StringVar(mots())
+Label(Frame1, Mots.set(lambda: inconnu(Mots.get(), Lettre))).pack(side = 'left')
+
+Largeur = 200
+Longueur = 200
+Canevas = Canvas(Frame1, width = Largeur, height = Longueur, bg = 'grey')
+Canevas.pack(padx = 5, pady = 5)
+#photo = PhotoImage(file = 'bonhomme8.gif')
+#item = Canevas.create_image(80,80, image = photo)
+Lettre = StringVar()
+Champ = Entry(Frame2, textvariable = Lettre, bg = 'white', fg = 'black')
+Champ.focus_set()
+Champ.pack(side = 'left', padx = 5, pady = 5)
+BoutonLettre = Button(Frame2, text = 'Entrez une lettre', 
+                      command = lambda:proposition(Lettre, Champ))
+BoutonLettre.pack(side = 'top', padx = 25, pady = 25)
+Label(Frame2, text = 'Lettres déjà utilisées : ').pack(side = 'left')
+
+
+
 Mafenetre.mainloop()
 
--- ma_lib_tk.py
+++ ma_lib_tk.py
@@ -7,8 +7,10 @@ Created on Wed Dec  9 17:25:37 2020
 """
 
 from random import choice
+from tkinter import messagebox
 
 lettres_mots = "abcdefghijklmnopqrstuvwxyz"
+lettres_utilisees = []
 
 #Cette fonction permet d'ouvrir le fichier texte où les mots sont contenus
 #et d'en choisir un au hasard parmis tous
@@ -23,16 +25,37 @@ def mots():
 
 #Cette fonction permet de remplacer les lettres à chercher par des _ et les
 #remplacer lorsque le joueur en a trouvé une
-def inconnu(mots, propositions):
+def inconnu(mots, Lettre):
     x = ""
     for i in mots:
-        if i in propositions:
+        if i in Lettre:
             x = x + i
         else:
             x = x + "_ "
     return x
 
+
 #Cette fonction permet de demander à l'utilisateur une lettre et elle sert de 
 #sécurité lorsque le joueur ne met pas un caractère approprié
-def proposition(propositions):
+def proposition(Lettre, Champ):
+    Lettres = Lettre.get()
+    Champ.delete(0, 'end')
+    print(Lettre)
+    if Lettres in lettres_mots:
+        lettres_utilisees.append(Lettres)
+        print(lettres_utilisees)
+    else:
+        messagebox.showwarning('Jeu du pendu', 
+                            "Choisissez une autre lettre, elle n'est pas valide")
+        
+#def partie():
+#    erreurs = 0
+#    mot = mots()
+#    propositions = []
+    
+    
+    
+    
+    
+    
     
